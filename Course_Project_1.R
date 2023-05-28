8a957d274475528d7e05a96036102a224281a44b
ydg <ydg983@naver.com>
2023-05-28 16:00:31 +0900

    commit for adding code

--- Course_Project_1.R
+++ Course_Project_1.R
@@ -1,9 +1,13 @@
 # datasets file is big, so execute code and download datasets on internet
 
 # Loading datasets
+if (!dir.exists('./datasets'))
+  dir.create('./datasets')
+
 url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 download.file(url, './datasets/household_power_consumption.zip')
 unzip('./datasets/household_power_consumption.zip', exdir='./datasets')
+
 
 datasets <- read.table('./datasets/household_power_consumption.txt', sep = ';', na.strings = "?", header=T, colClasses=c(rep("character",2),rep("numeric",7)))
 
